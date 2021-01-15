ab8a53f8cefacc837f79e722ddc7bb9a9fd4e69d
Савелий <savelussava@mail.ru>
2021-01-15 23:16:47 +0300

    Required to, replace every letter with its position in the alphabet.

---
+++ letter position/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letter_position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "The sunset sets at twelve o' clock.";
+            text = AlphabetPosition(text);
+        }
+        public static string AlphabetPosition(string text)
+        {
+            char[] sym = text.ToLower().ToCharArray();
+            string newtext = "";
+            foreach (var s in sym)
+            {
+                if (s >= 'a' && s <= 'z')
+                    newtext += (((int)s - 96).ToString() + ' ');
+            }
+            return newtext.Trim();
+        }
+    }
+    }
+}
